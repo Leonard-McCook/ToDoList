80f57d8a9564ec18069ea3e84ac7d63587d8ec81
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-06-29 14:39:50 -0400

    Create NewItemView and LoginView files

---
+++ ToDoList/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  ToDoList
+//
+//  Created by Leonard McCook-Carr on 6/29/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
