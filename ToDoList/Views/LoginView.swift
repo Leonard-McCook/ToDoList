5db6b8a54ea48480dbf45537652f9351ec86bd89
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-07-04 20:44:52 -0400

    Create login form

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct LoginView: View {
+
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView()
             
             // Login Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    // Attempt log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log in")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
+            }
             
             //Create Account
+            VStack {
+                Text("New around here?")
+                Button("Create and account") {
+                    // Show registration
+                    
+                }
+            }
+            .padding(.bottom, 50)
             
             Spacer()
         }
