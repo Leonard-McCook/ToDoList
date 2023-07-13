700afac2a1fc82ae362b3cafe285b02624727d17
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-07-13 18:08:01 -0400

    Create user object

--- ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Leonard McCook-Carr on 7/2/23.
 //
 
+import FirebaseAuth
 import Foundation
 
 class RegisterViewViewModel: ObservableObject {
@@ -15,10 +16,43 @@ class RegisterViewViewModel: ObservableObject {
     init() {}
     
     func register() {
+        guard validate() else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            self?.insertUserRecord(id: userId)
+        }
+    }
+    
+    private func insertUserRecord(id: String) {
+        let newUser = User(id: id,
+                            name: name,
+                            email: email,
+                            joined: Date().timeIntervalSince1970)
         
     }
     
-    private func validate() {
+    private func validate() -> Bool {
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
         
+        guard email.contains("@") && email.contains(".") else {
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            return false
+        }
+        
+        return true
     }
 }
+
