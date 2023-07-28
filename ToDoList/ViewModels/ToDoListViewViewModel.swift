f61c902401cc8cb3f2dba0903237920a0abc6bae
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-07-27 20:22:52 -0400

    Fully implement delete function of elements

--- ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Leonard McCook-Carr on 7/2/23.
 //
-
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for listof items view
@@ -12,10 +12,22 @@ import Foundation
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
     
-    init() {}
+    private let userId: String
     
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    /// Delete todo list item
+    /// - Parameter id: item id to delete
     func delete(id: String) {
+        let db = Firestore.firestore()
         
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
     }
 }
 
